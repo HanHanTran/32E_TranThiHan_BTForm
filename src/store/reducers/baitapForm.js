6ea4186d345427a3c790f8fbb218d49d1714f84d
HanHanTran <tranthihan2705@gmail.com>
2022-10-05 23:27:39 +0700

    upcode

---
+++ src/store/reducers/baitapForm.js
@@ -0,0 +1,49 @@
+const stateDefault = {
+   danhSachSinhVien: [
+//    {
+//     maSV:'',
+//     userName: '',
+//     email:'',
+//     phoneNumber:'',
+//    }
+   ],
+   selectedUsers: null,
+}
+export const baitapForm = (state= stateDefault ,{type,payload}) =>{
+    switch(type){
+        case 'ADD_USER':{
+            const data = [...state.danhSachSinhVien]
+            const user = {...payload,id: Date.now()}
+            data.push(user)
+            return {...state,danhSachSinhVien: data}
+        }
+        case 'DELETE_USER':{
+            const data = state.danhSachSinhVien.filter((item) => item.id !== payload)
+            return {...state,danhSachSinhVien: data}
+        }
+        case 'EDIT_USER':{
+            const user = state.danhSachSinhVien.find((item) => item.id === payload)
+            return {...state,selectedUsers: user}
+        }
+        case 'UPDATE_USER':{
+            const newUserList = state.danhSachSinhVien.map(item=>{
+                if(item.id === payload.id){
+                    return payload
+                }
+                return item
+            })
+            state.selectedUsers = null
+            return {...state,danhSachSinhVien:newUserList}
+            // const newUserList = state.danhSachSinhVien.map(item => item.id === payload.id ? payload:item)
+
+        }
+        case 'SREACH_USER':{
+            const data =state.danhSachSinhVien.filter(item=>item.maSV === payload)
+            return {...state,danhSachSinhVien:data}
+            
+        }
+        
+        default:
+            return state
+    }
+}
